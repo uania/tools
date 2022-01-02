b8c82b32d19e558b694e9444c346652a22aa002e
yifan.liu <uania.lau@gmail.com>
2022-01-02 20:47:14 +0800

    基于monoDevelop 的T4

---
+++ src/T4Framework/Uania.Tools.T4/T4Templates/Entities.cs
@@ -0,0 +1,17 @@
+﻿namespace Uania.Tools.Repository.Entities
+{
+    public class sports_testing_users
+    {
+        // 获取列信息
+        public string id {get; set;}
+        public string user_name {get; set;}
+        public string user_phone {get; set;}
+        public string user_email {get; set;}
+        public string user_state {get; set;}
+        public string user_role {get; set;}
+        public string is_delete {get; set;}
+        public string create_time {get; set;}
+        public string last_update_time {get; set;}
+    
+    }
+}      
